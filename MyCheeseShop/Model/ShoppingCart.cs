868c816b4c5792a32cd2cf230dc5fd3b993904a9
Sam Berner <samberner@gmail.com>
2024-03-26 20:32:04 +0000

    Fixed login and account issues with interactiveserver and static pages

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -3,7 +3,6 @@
     public class ShoppingCart
     {
         public event Action? OnCartUpdated;
-
         private List<CartItem> _items;
 
         public ShoppingCart()
@@ -26,7 +25,45 @@
 
         public int Count()
         { 
+            // return the number of items in the cart
             return _items.Count;
+        }
+
+        public decimal Total()
+        {
+            // sum the price of all items in the cart
+            return _items.Sum(item => item.Cheese.Price * item.Quantity);
+        }
+
+        public IEnumerable<CartItem> GetItems()
+        {
+            // return a copy of the items in the cart
+            return _items;
+        }
+
+        public void ChangeQuantity(Cheese cheese, int quantity)
+        {
+            // change the quantity of the cheese in the cart
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            if (item is not null)
+            {
+                item.Quantity = quantity;
+                OnCartUpdated?.Invoke();
+            }
+        }
+
+        public void RemoveItem(Cheese cheese)
+        {
+            // remove the cheese from the cart
+            _items.RemoveAll(item => item.Cheese.Id == cheese.Id);
+            OnCartUpdated?.Invoke();
+        }
+
+        public void Clear()
+        {
+            // remove all items from the cart
+            _items.Clear();
+            OnCartUpdated?.Invoke();
         }
     }
 }
